70081534965674a26fdd792d61bfa968e70301b9
XiaYu <191850205@smail.nju.edu.cn>
2022-05-03 23:15:20 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # llvm-compiler-demo
+
+本地jdk1.8有问题，请使用openjdk18版本
---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Main {
+
+  public static void main(String[] args){
+    CharStream input = CharStreams.fromString("a=(1+2)*3\na=a+3\n");
+    ExprLexer lexer = new ExprLexer(input);
+    ExprParser parser = new ExprParser(new CommonTokenStream(lexer));
+    ParseTree tree = parser.prog();
+    ExprVistor vistor = new ExprVistor();
+    vistor.visit(tree);
+    System.out.println("======================");
+    vistor.verify();
+    //System.out.println("======================");
+    //vistor.optimize();
+    System.out.println("======================");
+    vistor.exec();
+  }
+
+}
